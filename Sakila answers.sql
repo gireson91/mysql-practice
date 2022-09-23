cd5f3df340135f8a8373beb70dc1147d9f85f137
GIreson91 <gareth.ireson@nationwide.co.uk>
2022-09-23 11:28:13 +0100

    saved progress of sakila answers

--- Sakila answers.sql
+++ Sakila answers.sql
@@ -39,7 +39,23 @@ select first_name, last_name from actor where last_name="Neeson";
 select actor_id, first_name, last_name from actor where actor_id%10=0;
 
 #Q5 description of the movie with ID 100
-select `description` 
+select title, `description` from film where film_id=100;
 
+#Q6 every R rated movie
+select title, rating from film where rating="R";
+
+#Q7 every non R rated movie
+select title, rating from film where rating!="R";
+
+#Q8 10 shortest movies
+select title, length from film order by length asc limit 10;
+
+#Q9 find movies with longest runtime, without using limit
+select title, length from film where length=(select max(length) from film);
+
+#10 find all movies with deleted scenes
+select title, special_features from film where special_features like "%Deleted Scenes%";
+
+#11 using HAVING reverse-alphabetically list the last names that aren't repeated
 
 
